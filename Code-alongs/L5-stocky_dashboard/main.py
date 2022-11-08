80d1a242c4a5cbdf01aa25959f80b32551cc5948
Kokchun Giang <kokchungiang@gmail.com>
2022-11-08 10:54:05 +0100

    drawn plotly graph

--- Code-alongs/L5-stocky_dashboard/main.py
+++ Code-alongs/L5-stocky_dashboard/main.py
@@ -3,6 +3,8 @@ import dash_bootstrap_components as dbc
 import os
 from load_data import StockData
 from dash import html, dcc  # dcc - dash core components
+from dash.dependencies import Output, Input
+import plotly_express as px
 
 directory_path = os.path.dirname(__file__)
 path = os.path.join(directory_path, "stocksdata")
@@ -22,6 +24,12 @@ stock_options_dropdown = [
     {"label": name, "value": symbol} for symbol, name in symbol_dict.items()
 ]
 
+ohlc_options = [
+    {"label": option, "value": option} for option in ("open", "high", "low", "close")
+]
+
+slider_marks = {i: mark for i, mark in enumerate(["1 day", "1 week", "1 month", "3 months", "1 year", "5 year", "Max"])}
+
 print(df_dict.keys())
 # print(df_dict["TSLA"][0])
 
@@ -37,9 +45,23 @@ app.layout = html.Main(
             options=stock_options_dropdown,
             value="AAPL",
         ),
+        dcc.RadioItems(id="ohlc-radio", options=ohlc_options, value="close"),
+        dcc.Graph(id = "stock-graph"),
+        dcc.Slider(id = "time-slider", min = 0, max = 6, marks = slider_marks, value = 2, step = None)
     ]
 )
 
+@app.callback(
+    Output("stock-graph", "figure"),
+    Input("stockpicker-dropdown", "value"),
+    Input("ohlc-radio", "value"),
+    Input("time-slider", "value")
+)
+def update_graph(stock, ohlc, time_index):
+    # tuple unpacks a list 
+    dff_daily, dff_intraday = df_dict[stock]
+
+    return px.line(dff_daily, x = dff_daily.index, y = ohlc, title = symbol_dict[stock])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
